7f5c569b7b92211eb2e4daefdd598399003ba5af
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-20 10:18:59 +1100

    created server side function for 'About' section within the application.

---
+++ R/AboutApp.R
@@ -0,0 +1,11 @@
+#'@export
+about_app <- function() {
+  renderText(paste0("<p>Hello and welcome to the &apos;Coronavirus in Australia&apos; dashboard!</p>
+      <p><strong>The creator of this application is Mayunk Bharadwaj.</strong></p>
+      <p>The purpose of this application is to allow the user to interact with and manipulate data to gain an understanding of how the Coronavirus pandemic has affected Australia. There are three main components to the application:</p>
+      <p>1. Confirmed cases by State. The user is able to select a state of interest and generate a graph illustrating how confirmed coronavirus cases have changed over time.</p>
+      <p>2. Coronavirus Summary Table. The user is able to select a specific date and identify how many confirmed cases, cumulative confirmed cases, deaths, cumulative deaths, recoveries and cumulative recoveries occurred in Australia at the selected date.</p>
+      <p>3. Deaths in Victoria. The user is able to view a graph showing how many coronavirus-related deaths have occurred in Victoria over time. The user is able to click on any given data point to generate a sentence describing how many deaths occurred in Victoria on that particular day. The user is free to use this sentence in their own work.</p>
+      <p><br></p>")
+  )
+}
