436c48efe31ad6f3b233d3a7b453abd969a7b311
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-20 09:58:46 +1100

    created docs for stateSelectInput function

--- R/stateSelectInput.R
+++ R/stateSelectInput.R
@@ -1,3 +1,12 @@
+#'Refactored 'selectInput' ui code
+#'
+#'@description Allows user to select state/territory of choice within shiny app to generate graph of confirmed coronavirus cases over time.
+#'
+#'@return This function will not return anything within the console. Its purpose is to demonstrate the refactoring of the previous 'selectInput' ui code into a new 'stateSelectInput' function.
+#'
+#'@example
+#'stateSelectInput()
+#'
 #'@export
 stateSelectInput <- function() {
   selectInput("province",
