633a162d5d38593569a6689917591fedb61b2590
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-22 11:10:03 +1100

    populated roxygen sections

--- R/vicEventRenderText.R
+++ R/vicEventRenderText.R
@@ -1,9 +1,13 @@
-#'
+#'Refactored server-side output$VicDeathParagraph code into new function 'vicEventRenderText' function.
 #' 
-#' @description 
-#' @return 
+#' @description Within the shiny application, this function generates a sentence describing the date and number of deaths that occurred in Victoria related to coronavirus on that date by asking the user to click on any point on the Victorian coronavirus-related deaths graphic.
+#' 
+#' @return This function will not return anything meaningful for the end-user within the console. Its purpose is to demonstrate the refactoring of the previous 'output$VicDeathParagraph' server code into a new 'vicEventRenderText()' function. The code for the function may be used as a template by the user to include their own 'event_data' server code within a different shiny app.
 #' 
 #' @example
+#' \dontrun(
+#' vicEventRenderText()
+#' )
 #'
 #' 
 #' @import
