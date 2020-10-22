6baf4d8104ac63215caf9aebffa17f7e58e1c580
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-22 19:51:47 +1100

    modified data.R file and updated consequent files.

--- R/data.R
+++ R/data.R
@@ -2,9 +2,17 @@
 #'
 #' This dataset contains Australia's daily and cumulative coronavirus data for 2020
 #' 
-#' @description The dataset contains a date variable and corresponding variables for total 'Daily Confirmed' coronavirus cases, 'Daily Death' related to coronavirus, 'Daily Recovered' coronavirus cases, 'Cumulative Confirmed' coronavirus cases, 'Cumulative Deaths' related to coronavirus and 'Cumulative Recovered' coronavirus cases. The 'date' column is of type <date> and the remaining columns are of type <int>.
+#' @format a tibble with 192 observations and 7 variables:
 #' 
-#' @source
-#' The dataset is a processed version of Rami Krispin's c'oronavirus' dataset from the 'coronavirus package' which can be found here: https://github.com/RamiKrispin/coronavirus
+#' - **date**: the date on a particular day
+#' - **'Daily Confirmed'**: the daily confirmed coronavirus cases in Australia on that given date
+#' - **'Daily Death'**: the daily confirmed coronavirus deaths in Australia on that given date
+#' - **'Daily Recovered'**: the daily confirmed coronavirus drecoveries in Australia on that given date
+#' - **'Cumulative Confirmed'**: the cumulative confirmed coronavirus cases in Australia on that given date
+#' - **'Cumulative Deaths'**: the cumulative confirmed coronavirus deaths in Australia on that given date
+#' - **'Cumulative Recovered'**: the cumulative confirmed coronavirus recoveries in Australia on that given date
+#'  
+#' 
+#' @source The dataset is a processed version of Rami Krispin's 'coronavirus' dataset from the 'coronavirus package' which can be found here: https://github.com/RamiKrispin/coronavirus
 
 "oz_covid_data"
