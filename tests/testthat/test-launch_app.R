0a7c9685853f993a25c28bdc419b5328fb718d7d
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-22 13:17:45 +1100

    deleting expect_visible test

--- tests/testthat/test-launch_app.R
+++ tests/testthat/test-launch_app.R
@@ -6,8 +6,3 @@ library(ozcovid)
 test_that("error message appears if user tries to include an argument within the function", {
   expect_error(ozcovid::launch_app(hello))
 })
-
-
-test_that("application launches successfully",{
-  expect_visible(ozcovid::launch_app())
-})
