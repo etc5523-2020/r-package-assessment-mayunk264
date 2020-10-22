7e5acca87521b10b63f2f019c37f86ad2bae6c6e
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-22 11:54:55 +1100

    spelling changes

--- tests/testthat/test-stateSelectInput.R
+++ tests/testthat/test-stateSelectInput.R
@@ -2,7 +2,7 @@ context("Testing stateSelectInput function")
 library(ozcovid)
 library(shiny)
 
-test_that("error message appears if user tries to include an argument within the function - the function is not deisgned to be used within the console (only within shiny app)", {
+test_that("error message appears if user tries to include an argument within the function - the function is not designed to be used within the console (only within shiny app)", {
   expect_error(ozcovid::stateSelectInput("Victoria"))
 })
 
