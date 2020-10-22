d2685a22c6a4faad08f15019c7370ef5920c4da1
Mayunk Bharadwaj <mbha0014@student.monash.edu>
2020-10-22 13:20:52 +1100

    add test to stateSelectInput

--- tests/testthat/test-stateSelectInput.R
+++ tests/testthat/test-stateSelectInput.R
@@ -5,3 +5,7 @@ library(ozcovid)
 test_that("error message appears if user tries to include an argument within the function - the function is not designed to be used within the console (only within shiny app)", {
   expect_error(ozcovid::stateSelectInput("Victoria"))
 })
+
+test_that("the function executes silenty - no output. This is important as the function is meant to service the shiny app and not as a stand alone function for the end user.", {
+  expect_silent(ozcovid::stateSelectInput())
+})
